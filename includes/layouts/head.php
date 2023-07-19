11790616718b2c18dbe0f79e60caf1edff2f22d5
patrictobola <tobola92@gmail.com>
2023-07-19 15:22:44 +0200

    fixed head title

--- includes/layouts/head.php
+++ includes/layouts/head.php
@@ -2,5 +2,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css' integrity='sha512-t4GWSVZO1eC8BM339Xd7Uphw5s17a86tIZIj8qRxhnKub6WoyhnrxeCIMeAqBPgdZGlCcG2PrZjMc+Wr78+5Xg==' crossorigin='anonymous' />
-    <title><?php $title ?? 'Password Generator' ?></title>
+    <title><?= $title ?? 'Password Generator' ?></title>
+
 </head>
